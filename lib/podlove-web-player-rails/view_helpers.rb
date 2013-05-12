3913a83cae5e1e89cc5519f46ebe923d99cf84f4
Bastian Bartmann <xarfai27@gmail.com>
2013-05-12 17:49:44 +0200

    added podlovevideo view helper

--- lib/podlove-web-player-rails/view_helpers.rb
+++ lib/podlove-web-player-rails/view_helpers.rb
@@ -15,6 +15,19 @@ module Podlove
 
             return raw html + "\n" + javascript_tag(script)
           end
+
+          def podlovevideo(options = {})
+            html = "<video id='player'>"
+            html << "<source src='#{options[:src]}' type='#{options[:type]}'></source>"
+            html << "</video>"
+
+            options.delete(:src)
+            options.delete(:type)
+
+            script = "$('#player').podlovewebplayer(#{options.to_json});"
+
+            return raw html + "\n" + javascript_tag(script)
+          end
         end
       end
     end
