31fee8dc44958600b64d47a940b7618031e5692a
Bastian Bartmann <xarfai27@gmail.com>
2013-05-12 17:13:20 +0200

    added podloveaudio view helper

--- lib/podlove-web-player-rails.rb
+++ lib/podlove-web-player-rails.rb
@@ -1,10 +1,14 @@
 require "podlove-web-player-rails/version"
+require "podlove-web-player-rails/view_helpers"
 
 module Podlove
   module Web
     module Player
       module Rails
         class Engine < ::Rails::Engine
+          initializer "podlove-web-player-rails.view_helpers" do |app|
+            ActionView::Base.send :include, ViewHelpers
+          end
         end
       end
     end
---
+++ lib/podlove-web-player-rails/view_helpers.rb
@@ -0,0 +1,22 @@
+module Podlove
+  module Web
+    module Player
+      module Rails
+        module ViewHelpers
+          def podloveaudio(options = {})
+            html = "<audio id='player'>"
+            html << "<source src='#{options[:src]}' type='#{options[:type]}'></source>"
+            html << "</audio>"
+
+            options.delete(:src)
+            options.delete(:type)
+
+            script = "$('#player').podlovewebplayer(#{options.to_json});"
+
+            return raw html + "\n" + javascript_tag(script)
+          end
+        end
+      end
+    end
+  end
+end
