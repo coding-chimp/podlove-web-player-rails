327f800aac000f37c7a0f82049fa194de6d3bd3d
Bastian Bartmann <xarfai27@gmail.com>
2013-05-13 16:19:42 +0200

    move coveralls to the top of the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 require 'action_controller/railtie'
 require 'rails/test_unit/railtie'
 require 'podlove-web-player-rails'
-require 'coveralls'
-Coveralls.wear!
