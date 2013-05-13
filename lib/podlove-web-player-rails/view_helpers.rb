7ffe4fbdd3873c208a94682e98a0ed8761677f80
Bastian Bartmann <xarfai27@gmail.com>
2013-05-13 13:51:22 +0200

    removed duplication in view_helpers

--- lib/podlove-web-player-rails/view_helpers.rb
+++ lib/podlove-web-player-rails/view_helpers.rb
@@ -4,22 +4,19 @@ module Podlove
       module Rails
         module ViewHelpers
           def podloveaudio(options = {})
-            html = "<audio id='player'>"
-            html << "<source src='#{options[:src]}' type='#{options[:type]}'></source>"
-            html << "</audio>"
-
-            options.delete(:src)
-            options.delete(:type)
-
-            script = "$('#player').podlovewebplayer(#{options.to_json});"
-
-            return raw html + "\n" + javascript_tag(script)
+            podlove("audio", options)
           end
 
           def podlovevideo(options = {})
-            html = "<video id='player'>"
+            podlove("video", options)
+          end
+
+          private
+
+          def podlove(type, options = {})
+            html = "<#{type} id='player'>"
             html << "<source src='#{options[:src]}' type='#{options[:type]}'></source>"
-            html << "</video>"
+            html << "</audio>"
 
             options.delete(:src)
             options.delete(:type)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
-gem 'railties', '>= 3.0.0'
 require 'action_controller/railtie'
 require 'rails/test_unit/railtie'
-
 require 'podlove-web-player-rails'
 require 'coveralls'
 Coveralls.wear!
