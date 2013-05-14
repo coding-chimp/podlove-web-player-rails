29a94ab22b6d88e966a3323bc93d2efae49c7684
Bastian Bartmann <xarfai27@gmail.com>
2013-05-14 13:13:20 +0200

    added support for multiple codecs

--- lib/podlove-web-player-rails/version.rb
+++ lib/podlove-web-player-rails/version.rb
@@ -2,7 +2,7 @@ module Podlove
   module Web
     module Player
       module Rails
-        VERSION = "0.2.0.alpha"
+        VERSION = "0.3.0"
       end
     end
   end
--- lib/podlove-web-player-rails/view_helpers.rb
+++ lib/podlove-web-player-rails/view_helpers.rb
@@ -15,11 +15,22 @@ module Podlove
 
           def podlove(type, options = {})
             html = "<#{type} id='player'>"
-            html << "<source src='#{options[:src]}' type='#{options[:type]}'></source>"
-            html << "</audio>"
+            html << "<source src='#{options[:src]}'"\
+                    "" + (options[:type] ? " type='#{options[:type]}'" : "") + "></source>"      if options[:src]
+            html << "<source src='#{options[:mp3]}' type='audio/mpeg'></source>"                 if options[:mp3]
+            html << "<source src='#{options[:opus]}' type='audio/ogg; codecs=opus'></source>"    if options[:opus]
+            if type == 'audio'
+              html << "<source src='#{options[:mp4]}' type='audio/mp4'></source>"                if options[:mp4]
+              html << "<source src='#{options[:ogg]}' type='audio/ogg; codecs=vorbis'></source>" if options[:ogg]
+            end
+            html << "<source src='#{options[:opus]}' type='video/webm'></source>"                if options[:webm]
+            if type == 'video'
+              html << "<source src='#{options[:mp4]}' type='video/mp4'></source>"                if options[:mp4]
+              html << "<source src='#{options[:ogg]}' type='video/ogg'></source>"                if options[:ogg]
+            end
+            html << "</#{type}>"
 
-            options.delete(:src)
-            options.delete(:type)
+            [:src, :type, :mp4, :mp3, :ogg, :opus, :webm].each{ |key| options.delete(key) }
 
             script = "$('#player').podlovewebplayer(#{options.to_json});"
 
