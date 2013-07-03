bf33d09d27bbfa5daf75d0dda9a372e136822e0e
Bastian Bartmann <xarfai27@gmail.com>
2013-07-03 13:08:52 +0200

    updated podlove web player to v2.1.0

--- lib/podlove-web-player-rails/version.rb
+++ lib/podlove-web-player-rails/version.rb
@@ -2,5 +2,5 @@ module PodloveWebPlayerRails
   # version of the gem
   VERSION = "1.1.0"
   # version of the podlove web player
-  PODLOVE_WEB_PLAYER_VERSION = "2.0.11"
+  PODLOVE_WEB_PLAYER_VERSION = "2.1.0"
 end
