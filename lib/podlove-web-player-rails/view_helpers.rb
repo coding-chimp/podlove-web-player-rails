290686a6ddb9938a41f5c38ac4d5665a60b001b4
Bastian Bartmann <xarfai27@gmail.com>
2013-07-07 20:54:55 +0200

    fixed the preload option

--- lib/podlove-web-player-rails/view_helpers.rb
+++ lib/podlove-web-player-rails/view_helpers.rb
@@ -5,21 +5,22 @@ module PodloveWebPlayerRails
     # js method with given option parameters.
     #
     # @param [Hash] options options for the player
-    # @option options [String]  :src The location of an audio file, local ore remote.
-    # @option options [String]  :type The media type of the resource.
-    # @option options [String]  :mp4 The location of an audio file with the type mp4.
-    # @option options [String]  :mp3 The location of an audio file with the type mp3.
-    # @option options [String]  :ogg The location of an audio file with the type ogg.
-    # @option options [String]  :opus The location of an audio file with the type opus.
-    # @option options [String]  :poster The location of the cover image for the rich audio player.
+    # @option options [string]  :src The location of an audio file, local ore remote.
+    # @option options [string]  :type The media type of the resource.
+    # @option options [string]  :mp4 The location of an audio file with the type mp4.
+    # @option options [string]  :mp3 The location of an audio file with the type mp3.
+    # @option options [string]  :ogg The location of an audio file with the type ogg.
+    # @option options [string]  :opus The location of an audio file with the type opus.
+    # @option options [string]  :poster The location of the cover image for the rich audio player.
     # @option options [integer] :width (auto) The width of the player.
     # @option options [integer] :height The height of the player.
     # @option options [boolean] :loop (false) Loops the audio when it ends.
-    # @option options [boolean] :preload (true) Start loading the audio as soon as possible,
+    # @option options [string] :preload (auto) Start loading the video as soon as possible,
     #                           before the user clicks play. This might not work on all browsers.
+    #                           Options are: 'auto', 'metadata' and 'none'
     # @option options [boolean] :autoplay (false) Start playing the audio as soon as it's ready. 
     #                           This might not work on all (mobile) devices.
-    # @option options [String]  :duration (false) Enables display of duration without having to load the
+    # @option options [string]  :duration (false) Enables display of duration without having to load the
     #                           media file. Use seconds or timecode as a unit 
     #                           (e.g. "3522" or "00:58:42")
     # @option options [boolean] :alwaysShowHours (true) Displays the time in 00:00:00 instead of 00:00.
@@ -27,11 +28,11 @@ module PodloveWebPlayerRails
     #                           permanently visible.
     # @option options [boolean] :volume (true) Disables the volume slider.
     # @option options [boolean] :progress (true) Disables the progress bar.
-    # @option options [String]  :captions URL to a WebVTT captions file.
+    # @option options [string]  :captions URL to a WebVTT captions file.
     # @option options [boolean] :chapters Takes chapter string in JSON format and builds an interactive
     #                           chapter table. Chapters must be written in the following format:
     #                           !{'start':'00:00:00.000', 'title':'Chapter One', 'image':''}
-    # @option options [String]  :chapterlinks (all) Option for the jumplink behaviour in chapter table.
+    # @option options [string]  :chapterlinks (all) Option for the jumplink behaviour in chapter table.
     #                           Options are: 'all' (all chapter links are clickable), 'buffered' (only
     #                           buffered chapters are clickable), 'false' (chapters are not linked)
     # @option options [boolean] :chaptersVisible (false) Defines the default visibility status of
@@ -42,7 +43,7 @@ module PodloveWebPlayerRails
     #                           toggable summary module.
     # @option options [boolean] :downloadbuttonsVisible (false) Defines the default visibility of the
     #                           download buttons.
-    # @return [String] HTML markup and JS call.
+    # @return [string] HTML markup and JS call.
     def podloveaudio(options = {})
       podlove("audio", options)
     end
@@ -51,17 +52,18 @@ module PodloveWebPlayerRails
     # js method with given option parameters.
     #
     # @param [Hash] options options for the player
-    # @option options [String]  :src This location of an video file, local ore remote.
-    # @option options [String]  :type The media type of the resource.
-    # @option options [String]  :mp4 The location of an audio file with the type mp4.
-    # @option options [String]  :webm The location of an audio file with the type webm.
-    # @option options [String]  :ogg The location of an audio file with the type ogg.
-    # @option options [String]  :poster The location of the poster frame for the video.
+    # @option options [string]  :src This location of an video file, local ore remote.
+    # @option options [string]  :type The media type of the resource.
+    # @option options [string]  :mp4 The location of an audio file with the type mp4.
+    # @option options [string]  :webm The location of an audio file with the type webm.
+    # @option options [string]  :ogg The location of an audio file with the type ogg.
+    # @option options [string]  :poster The location of the poster frame for the video.
     # @option options [integer] :width (auto) The width of the player.
     # @option options [integer] :height The height of the player.
     # @option options [boolean] :loop (false) Loops the video when it ends.
-    # @option options [boolean] :preload (true) Start loading the video as soon as possible,
+    # @option options [string] :preload (auto) Start loading the video as soon as possible,
     #                           before the user clicks play. This might not work on all browsers.
+    #                           Options are: 'auto', 'metadata' and 'none'
     # @option options [boolean] :autoplay (false) Start playing the video as soon as it's ready.
     #                           This might not work on all (mobile) devices.
     # @option options [boolean] :fullscreen (true) Disables the fullscreen button for video.
@@ -74,11 +76,11 @@ module PodloveWebPlayerRails
     #                           controls out when not hovering the video.
     # @option options [boolean] :volume (true) Disables the volume slider.
     # @option options [boolean] :progress (true) Disables the progress bar.
-    # @option options [String]  :captions URL to a WebVTT captions file.
+    # @option options [string]  :captions URL to a WebVTT captions file.
     # @option options [boolean] :chapters Takes chapter string in JSON format and builds an interactive
     #                           chapter table. Chapters must be written in the following format:
     #                           !{'start':'00:00:00.000', 'title':'Chapter One', 'image':''}
-    # @option options [String]  :chapterlinks (all) Option for the jumplink behaviour in chapter table.
+    # @option options [string]  :chapterlinks (all) Option for the jumplink behaviour in chapter table.
     #                           Options are: 'all' (all chapter links are clickable), 'buffered' (only
     #                           buffered chapters are clickable), 'false' (chapters are not linked)
     # @option options [boolean] :chaptersVisible (false) Defines the default visibility status of
@@ -89,7 +91,7 @@ module PodloveWebPlayerRails
     #                           toggable summary module.
     # @option options [boolean] :downloadbuttonsVisible (false) Defines the default visibility of the
     #                           download buttons.
-    # @return [String] HTML markup and JS call.
+    # @return [string] HTML markup and JS call.
     def podlovevideo(options = {})
       podlove("video", options)
     end
@@ -98,8 +100,9 @@ module PodloveWebPlayerRails
 
     def podlove(type, options = {})
       id = "player_" + SecureRandom.hex(5)
+      preload ||= "auto"
   
-      html = "<#{type} id='#{id}' preload=\"none\">"
+      html = "<#{type} id='#{id}' preload=#{preload}>"
       html << "<source src='#{options[:src]}'"\
               "" + (options[:type] ? " type='#{options[:type]}'" : "") + "></source>"      if options[:src]
       html << "<source src='#{options[:mp4]}' type='#{type}/mp4'></source>"                if options[:mp4]
@@ -109,7 +112,7 @@ module PodloveWebPlayerRails
       html << "<source src='#{options[:webm]}' type='#{type}/webm'></source>"              if options[:webm]
       html << "</#{type}>"
   
-      [:src, :type, :mp4, :mp3, :ogg, :opus, :webm].each{ |key| options.delete(key) }
+      [:src, :type, :mp4, :mp3, :ogg, :opus, :webm, :preload].each{ |key| options.delete(key) }
   
       script = "jQuery('##{id}').podlovewebplayer(#{options.to_json});"
   
